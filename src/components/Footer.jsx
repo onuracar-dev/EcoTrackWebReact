85ce302052bdf65534175fca648bd3aa571eea3c
Onur Acar <onur@ecotrack.org>
2026-06-08 03:53:39 +0300

    add : Footer and home page video background

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="ecoFooter justify-content-center text-center">
+      <p>© 2026 EkoTrack Projesi • Yeşili Koru, Geleceği Kurtar.</p>
+    </footer>
+  );
+}
+
+export default Footer;
